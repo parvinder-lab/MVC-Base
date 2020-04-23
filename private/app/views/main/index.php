44a3f4e68311131b51bac0a87e67ab0a52bb53b1
a00210586 <parvindersingh559900@gmail.com>
2020-04-23 23:00:31 +0000

    changes by sir

--- private/app/views/main/index.php
+++ private/app/views/main/index.php
@@ -2,7 +2,7 @@
 
 <ul>
 <?php
-foreach($posts as $post){
+foreach($posts as $post) {
     echo("<li><a href=\"\\blog\\read\\" . $post["slug"] . "\">" . $post["title"] . "<\a> - <time>" . $post["post_date"] . "</time>");
 
 }
