92670f36a8a09c3d19a4e0b0c27919c6d5e23d45
a00210586 <parvindersingh559900@gmail.com>
2020-04-25 04:38:04 +0000

    login

--- private/app/views/test/login.php
+++ private/app/views/test/login.php
@@ -1,10 +1,14 @@
 
-
-
+<style>
+form > * {
+    display:block;
+}
+</style>
 <form method="POST" action= "/test/login">
 <input type ="hidden" name="csrf" value="<php echo($csrf) ?>">
- <label for="name">Email</label>
- <input id="name" name = "name" required autocomplete="email">
+ <label for="username">Email</label>
+ <input id="username" name = "username" required autocomplete="email">
  <label for = "password">Password</label>
  <input type = "password" id = "password" name = "password" required autocomplete="email">
+ <input type="submit" value="Login">
  </form>
